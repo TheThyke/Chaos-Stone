11f53bc2dad965656dd98484a99548f67ea77207
TheThykeX <thyke.gaming@hotmail.com>
2019-06-07 12:13:50 +0300

    Create ChaosStoneHeader.h

---
+++ ChaosStoneHeader.h
@@ -0,0 +1,6 @@
+  bool ChaosStoneLoad();
+	void ChaosStoneRespawnTimer();
+	void ChaosStoneSummon(uint16 ChaosGetID,uint8 RankID,uint16 ZoneID);
+	uint8 ChaosStoneSummonSelectStage(uint16 ChaosGetID, uint8 RankID, uint16 ZoneID);
+  void	ChaosStoneDeath(CUser *pUser);
+	uint8	ChaosStoneSelectStage(uint8 sRank);
