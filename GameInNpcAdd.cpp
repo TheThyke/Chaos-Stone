6dfd932c5e4073c9d2f731c94a5572bed06344d8
TheThykeX <thyke.gaming@hotmail.com>
2019-06-08 22:23:17 +0300

    Create GameInNpcAdd.cpp

---
+++ GameInNpcAdd.cpp
@@ -0,0 +1,31 @@
+if (pNpc->GetZoneID() == ZONE_RONARK_LAND
+  || pNpc->GetZoneID() == ZONE_RONARK_LAND_BASE
+  || pNpc->GetZoneID() == ZONE_ARDREAM)
+{
+  if (NpcAddSignalling->m_ChaosStoneChaosID > 0)
+  {
+    _CHAOS_STONE_INFO* pChaosInfo = g_pMain->m_ChaosStoneInfoArray.GetData(addNPCSignal->m_ChaosStoneChaosID);
+    if (pChaosInfo != nullptr)
+    {
+      if (pNpc->GetZoneID() == pChaosInfo->sZoneID && pChaosInfo->isChaosStoneKilled == true && pNpc->isMonster())
+      {
+        if (pChaosInfo->sBoosID[0] == 0)
+          pChaosInfo->sBoosID[0] = pNpc->GetID();
+        else if (pChaosInfo->sBoosID[1] == 0)
+          pChaosInfo->sBoosID[1] = pNpc->GetID();
+        else if (pChaosInfo->sBoosID[2] == 0)
+          pChaosInfo->sBoosID[2] = pNpc->GetID();
+        else if (pChaosInfo->sBoosID[3] == 0)
+          pChaosInfo->sBoosID[3] = pNpc->GetID();
+        else if (pChaosInfo->sBoosID[4] == 0)
+          pChaosInfo->sBoosID[4] = pNpc->GetID();
+        else if (pChaosInfo->sBoosID[5] == 0)
+          pChaosInfo->sBoosID[5] = pNpc->GetID();
+        else if (pChaosInfo->sBoosID[6] == 0)
+          pChaosInfo->sBoosID[6] = pNpc->GetID();
+
+        pChaosInfo->sBoosKilledCount++;
+      }
+    }
+  }
+}
