4786b40cc2fb9716735b89c12d4aa5f37f2b510f
TheThykeX <thyke.gaming@hotmail.com>
2019-06-08 22:16:13 +0300

    Update ChaosStoneHeader.h

--- ChaosStoneHeader.h
+++ ChaosStoneHeader.h
@@ -1,6 +1,9 @@
-  bool ChaosStoneLoad();
-	void ChaosStoneRespawnTimer();
-	void ChaosStoneSummon(uint16 ChaosGetID,uint8 RankID,uint16 ZoneID);
-	uint8 ChaosStoneSummonSelectStage(uint16 ChaosGetID, uint8 RankID, uint16 ZoneID);
-  void	ChaosStoneDeath(CUser *pUser);
-	uint8	ChaosStoneSelectStage(uint8 sRank);
+bool ChaosStoneLoad();
+void ChaosStoneRespawnTimer();
+void ChaosStoneSummon(uint16 ChaosGetID,uint8 RankID,uint16 ZoneID);
+uint8 ChaosStoneSummonSelectStage(uint16 ChaosGetID, uint8 RankID, uint16 ZoneID);
+uint8 ChaosStoneSummonSelectFamilyStage(uint16 ChaosGetID, uint8 FamilyID, uint16 ZoneID);
+void	ChaosStoneDeath(CUser *pUser);
+void	ChaosStoneBossKilledBy();
+uint8	ChaosStoneSelectStage(uint8 sRank);
+void    ChaosStoneDeathRespawnMonster(uint16 ChaosGetIndex);
